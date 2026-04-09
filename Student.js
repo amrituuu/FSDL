0323f2eba7d838a904b80a95f957735ced4c9221
amrituuu <amritj2510@gmail.com>
2026-04-09 19:37:45 +0530

    Add files via upload

---
+++ Student.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  rollNo: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  confirmPassword: {
+    type: String,
+    required: true
+  },
+  contactNumber: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("Student", studentSchema);
